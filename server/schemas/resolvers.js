063b420c55b891e689937b312cb22a1d40ed420c
Kyle Richnafsky <7krich@gmail.com>
2022-06-22 14:08:37 -0400

    defined Thought query and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,20 @@
+const { User, Thought } = require('../models');
+
 // SERVE RESPONSE FOR typDefs query
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+            // use parent has placeholder parameter
+            // access username argument from second param
+        thoughts: async (parent, { username }) => {
+            // user ternary operator to check if username exists
+            // if it doesn't return empty obj
+            const params = username ? { username } : {};
+            // use find to sort through Thought model
+            // then sort in descending order
+            return Thought.find().sort({ createdAt: -1 })
+        }
     }
-  };
+};
   
   module.exports = resolvers;
